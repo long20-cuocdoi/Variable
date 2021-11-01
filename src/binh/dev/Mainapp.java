487a1cdbb4e013d45aea8afeb39f432c72ada7df
Your Name <you@example.com>
2021-11-01 14:53:24 +0700

    first commit

---
+++ src/binh/dev/Mainapp.java
@@ -0,0 +1,32 @@
+package binh.dev;
+/**
+ * 
+ * @author Administrator
+ *
+ */
+
+public class Mainapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * Khai báo biến
+		 * 
+		 */
+		String firstName = "Phan Van";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		
+		System.out.println("Ho Va Ten:");
+		System.out.println(firstName + " " + lastName );
+		System.out.println("Mark Math: " + markMath );
+		System.out.println("Passed: " + passed);
+		
+		//Khai báo hằng. Hằng phải viết hoa
+		final float PI = 3.14f;
+		
+	}
+
+}
